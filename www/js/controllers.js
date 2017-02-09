655d1be8cfb0d9e501aea3edd04f00e4ac597d54
Bonnin Luc <lucbonnin@gmail.com>
2017-02-09 10:02:31 +0100

    add debug console info + comment

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,17 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {
 
    $scope.launchComet = function (){
-      ReadyUI.loginWithUsername("demo1","user1",function (result){});
+      console.debug('launchComet() : will try to launch CometChat ReadyUI page');
+	  if(typeof(ReadyUI) == 'undefined') {
+	     alert('ReadyUI is undefined. Are you trying to launch cometchat without cordova ?');
+ 		 console.error('ReadyUI is undefined. Are you trying to launch cometchat without cordova ?');
+	  }
+      else {
+	      // Will login as demo1 user, granted for cometchat demo server (check app.js for ReadyUI config server init)
+		  ReadyUI.loginWithUsername("demo1","user1",function (result){
+			console.debug('launchComet() : CometChat launched successfully');
+		  });
+	  }
     }
     $scope.loginCallBack = function (result){
       // alert("loginCallBack");
